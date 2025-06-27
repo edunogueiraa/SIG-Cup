56788562b3c9f2ddf1c398d6b2fa77653287e1b5
edunogueiraa <eduardonogueira105@gmail.com>
2025-06-27 00:08:21 -0300

    Implementação da V3

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 opcao = ''
 while opcao != '0':
-    
+    os.system('clear')
+
     print("___________________________________________________")
     print("|                                                 |")
     print("|                Sistema SIG-Cup                  |")
@@ -15,14 +16,12 @@ while opcao != '0':
     print("|           0 Sair                                |")
     print("|_________________________________________________|")
     print("\n")
-    opcao = int(input("Digite a opção desejada: "))
+    opcao = str(input("Digite a opção desejada: "))
 
     match opcao:
 
-        case 1:
-         op_clientes = ''
-         while op_clientes != '0':
-
+        case '1':
+            os.system('clear')
             print("____________________________________________")
             print("|                                          |")
             print("|                Clientes                  |")
@@ -35,12 +34,12 @@ while opcao != '0':
             print("|           5 Listar                       |")
             print("|           0 Sair                         |")
             print("|__________________________________________|")
-            op_clientes = input("Escolha sua opção: ")
+            op_clientes = str(input("Escolha sua opção: "))
 
-            match op_clientes != 0:
+            match op_clientes != '0':
 
-                case 1:
-
+                case '1':
+                    os.system('clear')
                     print("____________________________________________")
                     print("|                                          |")
                     print("|           Cadastro de Clientes           |")
@@ -54,11 +53,11 @@ while opcao != '0':
                     input("Tecle <ENTER> para continuar...")
 
 
-                case 2:
-
+                case '2':
+                    os.system('clear')
                     print("____________________________________________")
                     print("|                                          |")
-                    print("|           Exibir Dados de Clientes       |")
+                    print("|          Exibir Dados dos Clientes       |")
                     print("|__________________________________________|\n")
 
                     print("Nome: ")
@@ -67,14 +66,37 @@ while opcao != '0':
 
                     input("Tecle <ENTER> para continuar...")
 
-                case 3:
-                 print()
-                case 4:
-                 print()
-                case 0:
-                 print()
+                case '3':
+                    os.system('clear')
+                    print("____________________________________________")
+                    print("|                                          |")
+                    print("|         Atualizar Dados dos Clientes     |")
+                    print("|__________________________________________|\n")
+                    
+                    nome = str(input("Nome: "))
+                    endereco = str(input("\nEndereço: "))
+                    telefone = str(input("\nTelefone: "))
 
-        case 2:
+                    print("\n\nDados alterados com sucesso!\n")
+                    input("Tecle <ENTER> para continuar...")
+
+                case '4':
+                    os.system('clear')
+                    print("____________________________________________")
+                    print("|                                          |")
+                    print("|           Excluir Dados dos Clientes     |")
+                    print("|__________________________________________|\n")
+
+                    valor_id = input("Digite o id do cliente para excluir: \n")
+
+                    print("Cliente excluido com sucesso!")
+                    input("Tecle <ENTER> para continuar...")
+
+                case '0':
+                    print("Saindo do Módulo Cliente")
+                    
+        case '2':
+            os.system('clear')
             print("____________________________________________")
             print("|                                          |")
             print("|                Canecas                   |")
@@ -86,9 +108,38 @@ while opcao != '0':
             print("|           4 Deletar                      |")
             print("|           0 Sair                         |")
             print("|__________________________________________|")
-            Resposta = input("Escolha sua opção: ")
+            op_canecas = str(input("Escolha sua opção: "))
 
-        case 3:
+            match op_canecas != '0':
+
+                case '1':
+                    os.system('clear')
+                    print("____________________________________________")
+                    print("|                                          |")
+                    print("|           Cadastro de Canecas            |")
+                    print("|__________________________________________|\n")
+
+                    cor  = str(input("Cor: "))
+                    valor = float(input("\nvalor: "))
+                    id = id + 1 #Ideia de uma fila de IDs
+
+                    print("\n\nCaneca cadastrada com sucesso!\n")
+                    input("Tecle <ENTER> para continuar...")
+
+                case '2':
+                    print("Listar")
+                
+                case '3':
+                    print("Atualizar")
+                
+                case '4':
+                    print("Deletar")
+                
+                case '0':
+                    print("Saindo do módulo canecas")
+
+        case '3':
+            os.system('clear')
             print("____________________________________________")
             print("|                                          |")
             print("|                Pedidos                   |")
@@ -100,10 +151,28 @@ while opcao != '0':
             print("|           4 Deletar                      |")
             print("|           0 Sair                         |")
             print("|__________________________________________|")
-            Resposta = input("Escolha sua opção: ")
+            op_pedidos = str(input("Escolha sua opção: "))
 
-        case 4:
+            match op_pedidos != 0:
 
+                case '1':
+                    print("Cadastrar")
+
+                case '2':
+                    print("Listar")
+                
+                case '3':
+                    print("Atualizar")
+                
+                case '4':
+                    print("Deletar")
+                
+                case '0':
+                    print("Saindo do módulo pedidos")
+
+
+        case '4':
+            os.system('clear')
             print("____________________________________________")
             print("|                                          |")
             print("|                Informações               |")
@@ -113,9 +182,28 @@ while opcao != '0':
             print("|    GitHub: @edunogueiraa                 |")
             print("|    Email: eduardonogueira105@gmail.com   |")
             print("|__________________________________________|")
-            Resposta = input("Escolha sua opção: ")
+            op_informacoes = str(input("Escolha sua opção: "))
 
-        case 0:
+
+            match op_informacoes != 0:
+
+                case '1':
+                    print("Cadastrar")
+                    
+                case '2':
+                    print("Listar")
+                
+                case '3':
+                    print("Atualizar")
+                
+                case '4':
+                    print("Deletar")
+                
+                case '0':
+                    print("Saindo do módulo informações")
+
+
+        case '0':
             print("Encerrando o programa")
             input("Tecle <ENTER> para continuar...")
 
